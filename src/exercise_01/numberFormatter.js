9b94ee7d31d6aa2f05573e087836ab691c706060
SONGYI-chen123 <song1577660501@163.com>
2020-09-07 15:18:00 +0800

    [Yichen Song]feat:exercise-01 finished

--- src/exercise_01/numberFormatter.js
+++ src/exercise_01/numberFormatter.js
@@ -7,6 +7,10 @@ export default function formatNumber(number, option) {
   //
   // * Please implement the function and pass all the tests in format_number_spec.js.
   // * Please do NOT modify the signature of the function.
+  if (option === undefined || option.currency === false) {
+    return `${number.toFixed(2)}`;
+  }
+  return `$ ${number.toFixed(2)}`;
 
   throw new Error('Please delete this line and implement the function');
 }
