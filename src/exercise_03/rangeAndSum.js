7b0e700646bf1431f3fb7ec87ffed1c3cfc8f4b4
SONGYI-chen123 <song1577660501@163.com>
2020-09-07 16:05:40 +0800

    [Yichen Song]feat:exercise-03 finished

--- src/exercise_03/rangeAndSum.js
+++ src/exercise_03/rangeAndSum.js
@@ -6,7 +6,18 @@ export function range(start, end) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
+  let arr = [];
+  if ( start > end ) {
+    for (let i = start; i > end ; i -= 1) {
+      arr.push(i);
+    }
+  }
+  if ( start < end) {
+    for (let i = start ; i < end ; i += 1) {
+      arr.push(i);
+    }
+  }
+  return arr;
   throw new Error('Please delete this line and implement the function');
 }
 
@@ -17,6 +28,11 @@ export function sum(...numbers) {
   //
   // * Please implement this function and pass all the tests in range_and_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
+  let Addsum = 0;
+  for(let i =0;i<numbers.length;i+=1)
+  {
+    Addsum += numbers[i];
+  }
+  return Addsum;
   throw new Error('Please delete this line and implement the function');
 }
